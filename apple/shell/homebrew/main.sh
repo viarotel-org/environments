728a5cceed5dbe42865fcf426e26cc7472ee4a6e
viarotel <viarotel@qq.com>
2023-05-13 19:57:14 +0800

    update

--- apple/shell/homebrew/main.sh
+++ apple/shell/homebrew/main.sh
@@ -23,9 +23,9 @@ function import_remote_script() {
 }
 
 # 安装 homebrew
-import_remote_script 'https://gitee.com/viarotel/environments/raw/main/apple/shell/homebrew/install.sh'
+import_remote_script 'https://gitee.com/viarotel/environments/raw/gitee/apple/shell/homebrew/install.sh'
 # 恢复 Brewfile 备份文件
-import_remote_script 'https://gitee.com/viarotel/environments/raw/main/apple/shell/homebrew/restore.sh'
+import_remote_script 'https://gitee.com/viarotel/environments/raw/gitee/apple/shell/homebrew/restore.sh'
 
 if [[ "$ZSH_EVAL_CONTEXT" == "toplevel" ]]; then
   exit 0
